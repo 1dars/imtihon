5a31b9d67b6d6eb797e913f0e9980f1c9aec5b56
1dars <jumadullaeviskandar@gmail.com>
2024-01-23 17:30:32 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# a = [4,3,2,1]
+# def calc():
+#     a.sort()
+#     print(a)
+# calc()
+
+# a = [7,8,11,66]
+# def calc():
+#     a.reverse()
+#     print(a)
+# calc()
+a = [1,2,3,4]
+def calc():
+    print(a)
+calc()
+
+
+
+
+
